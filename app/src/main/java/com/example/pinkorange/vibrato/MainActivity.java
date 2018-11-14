507bb9a2fdb89dce1038eb8e5219b51a3c3bdaa6
Janaki Vivrekar <janaki.vivrekar@berkeley.edu>
2018-11-14 12:01:05 -0800

    add recorded music button on MainActivity screen

--- app/src/main/java/com/example/pinkorange/vibrato/MainActivity.java
+++ app/src/main/java/com/example/pinkorange/vibrato/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.pinkorange.vibrato;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(MainActivity.this, LiveWithSettings.class);
-        startActivity(intent);
+        Button recorded_music_button = findViewById(R.id.recorded_music_button);
+        recorded_music_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LiveWithSettings.class);
+                startActivity(intent);
+            }
+        }
+        );
     }
 }
