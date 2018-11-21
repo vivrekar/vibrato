e8db2d34e6d61c5f4e604d618989efb479e9791f
Revekka Kostoeva <irmareka@gmail.com>
2018-11-21 04:00:12 -0800

    adding custom settings menu, loudness, disclaimer, moving my bass+loudness to live settings

--- app/src/main/java/com/example/pinkorange/vibrato/MainActivity.java
+++ app/src/main/java/com/example/pinkorange/vibrato/MainActivity.java
@@ -7,27 +7,20 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    protected Intent recordedIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button recorded_music_button = findViewById(R.id.recorded_music_button);
-        recordedIntent = new Intent(MainActivity.this, SelectMusic.class);
-        recorded_music_button.setOnClickListener(new View.OnClickListener() {
-             public void onClick(View v) {
-                 Intent intent = new Intent(MainActivity.this, SelectMusic.class);
-                 startActivity(intent);
-             }
-         });
+        Intent intent = new Intent(MainActivity.this, LiveWithSettings.class);
+        startActivity(intent);
 
         Button bass = findViewById(R.id.buttonBass);
         bass.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v)
             {
-                Intent i = new Intent(MainActivity.this, BassBooster.class);
+                Intent i = new Intent(getApplicationContext(), BassBooster.class);
                 startActivity(i);
             }
         });
