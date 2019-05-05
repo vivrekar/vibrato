1dec9a7fcaf3d2e5bf62527a25a8306a5ad74586
zzyyzzzzy <40347232+zzyyzzzzy@users.noreply.github.com>
2019-05-05 14:40:17 -0700

    Refactoring and publish
    App Published...

---
+++ app/src/main/java/com/music/pinkorange/vibrato/FixedTopFadeEdgeScrollView.java
@@ -0,0 +1,42 @@
+package com.music.pinkorange.vibrato;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
+
+public class FixedTopFadeEdgeScrollView extends ScrollView {
+
+        public FixedTopFadeEdgeScrollView(Context context) {
+            super(context);
+        }
+        private boolean enableScrolling = true;
+        public FixedTopFadeEdgeScrollView(Context context, AttributeSet attrs) {
+            super(context, attrs);
+        }
+
+        public FixedTopFadeEdgeScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+            super(context, attrs, defStyleAttr);
+        }
+
+        @Override
+        protected float getTopFadingEdgeStrength() {
+            return 0.1f;
+        }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (scrollingEnabled()) {
+            return super.onTouchEvent(ev);
+        } else {
+            return false;
+        }
+    }
+    private boolean scrollingEnabled(){
+        return enableScrolling;
+    }
+    public void setScrolling(boolean enableScrolling) {
+        this.enableScrolling = enableScrolling;
+    }
+}
+
---
+++ app/src/test/java/com/music/pinkorange/vibrato/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.music.pinkorange.vibrato;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit croatian, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
